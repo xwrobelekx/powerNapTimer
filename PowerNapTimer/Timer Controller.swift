cb83966f24b4e357371b0fb0d7b1df672304e7d6
Kamil Wrobel <wrobelk@live.com>
2018-08-28 14:53:34 -0600

    Implemented all the logic for the app

--- PowerNapTimer/Timer Controller.swift
+++ PowerNapTimer/Timer Controller.swift
@@ -7,9 +7,71 @@
 //
 
 import Foundation
+import UserNotifications
+
+//#1 step one declare protocol
+protocol TimerControllerDelegate: class{
+    //#4
+    var identifier : String {get}
+    var timeRemaining: TimeInterval? {get set}
+    
+    func timerSecondTick()
+    func timerCompleted()
+    func timerStopped()
+    
+    
+}
+
+
+extension TimerControllerDelegate {
+    
+    //default inplemetation of a function
+    func scheduleLocalNotification(){
+        
+        //#9 content for notification request
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = "Wake UP"
+        notificationContent.body = "No... Seriously Wake up"
+        
+        guard let timeRemaining = TimerController.shared.timeRemaining else {return}
+        
+        //#8 date "since" return the date of when the code runs
+        let date = Date(timeInterval: timeRemaining, since: Date())
+        
+        //#7
+        let dateComponents = Calendar.current.dateComponents([.minute, .second], from: date)
+        
+        //#6
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        //#2.1 every notification need unique identifier uuid set it in protocol and confrom to it in VC
+        let request = UNNotificationRequest(identifier: identifier, content: notificationContent, trigger: trigger)
+        
+        
+        //#1
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Unable to add Notification Request. \(error), \(error.localizedDescription)")
+            }
+        }
+        //#2 lookup UNNotificationrequest
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
 
 
 class TimerController {
+    //#3
+    var identifier = "PowerNapTimerIdentyfier"
     var timeRemaining: TimeInterval?
     var timer: Timer?
     
@@ -27,6 +89,9 @@ class TimerController {
         }
     }
     
+    //#2 the hook?
+    weak var delegate : TimerControllerDelegate?
+    
     
     //presents time in a readable to human manner as a string
     func timeAsString() -> String{
@@ -36,10 +101,12 @@ class TimerController {
         //pull minutes form time remaining
         let minutes = timeRemaining / 60
         //pull the remaining seconds
-        let seconds = timeRemaining - (minutes / 60)
+        let seconds = timeRemaining - (minutes * 60)
         
-        
-        return "\(minutes) : \(seconds)"
+        //this is an initializer for string, which return better formated string
+        // the 2 stands for how many digits, and d replaces the value with the one from array
+        //and array holds the minutes and seconds
+        return String(format: "%02d : %02d", arguments: [minutes, seconds])
     }
     
     //what to do as the timer takes on
@@ -53,6 +120,9 @@ class TimerController {
             
             print("time is running....")
             self.timeRemaining = timeRemaining - 1
+            //#3 notifies the parent that the second goes by
+            delegate?.timerSecondTick()
+            
             print(timeRemaining)
             
             
@@ -61,8 +131,12 @@ class TimerController {
             //need to stop the timer
             print("runned out of seconds, - stopping timer")
             timer?.invalidate()
+            
+            
             //set the time remaining to nil not 0
             self.timeRemaining = nil
+            //#3.2 notifie the paternt that the timer completed
+            delegate?.timerCompleted()
             
         }
         
@@ -103,6 +177,8 @@ class TimerController {
             
             //reset the remaining time
             timeRemaining = nil
+            //#3.3 notifie the parent that the timer stopped
+            delegate?.timerStopped()
         }
         
         
--- PowerNapTimer/TimerViewController.swift
+++ PowerNapTimer/TimerViewController.swift
@@ -7,25 +7,136 @@
 //
 
 import UIKit
-
-class TimerViewController: UIViewController {
+//#4 adopt the protocol
+class TimerViewController: UIViewController, TimerControllerDelegate {
     
-    //after declaring all the functionality of the timer in timer controller, we need to work on presenting or cominicating  with them
+    
+    
+    //#5 
+    var identifier = "PowerNapTimerIdentyfier"
+    var timeRemaining: TimeInterval? = TimerController.shared.timeRemaining
+    
+    //after declaring all the functionality of the timer in timer controller, we need to work on presenting or comunicating  with them
     
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var timerButtonLabel: UIButton!
     
+  
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        TimerController.shared.startTimer(time: 20)
+        
+        //this is saying that i will be your delegate, i will be your boss
+        TimerController.shared.delegate = self
+        
+    }
+//helper function to update labels
+    func updateView() {
+        updateButton()
+        updateTimerLabel()
+    }
+    
+    //updates the button based on if the timer is running or not
+    func updateButton(){
+        if TimerController.shared.isOn {
+            //in order to change button text u need to use this, wont work on text proerty
+            timerButtonLabel.setTitle("Stop Timer", for: .normal)
+        } else {
+            timerButtonLabel.setTitle("Start Timer", for: .normal)
+        }
+        
+    }
+    
+    
+    
+    
+    func updateTimerLabel(){
+        // we can use the func that we had in timer controller that returnet time as sting
+        timeLabel.text = TimerController.shared.timeAsString()
+        
     }
 
-
-
-
+    // we want to start and top the timer in the middle of running
     @IBAction func timerButtonTapped(_ sender: Any) {
+        let timerIsOn = TimerController.shared.isOn
+        
+        //if timer is running we cans stop it
+        if timerIsOn {
+            TimerController.shared.stopTimer()
+            
+        } else {
+        
+        TimerController.shared.startTimer(time: 5)
+            //call the notification
+            scheduleLocalNotification()
+        }
+        
+        updateView()
     }
+    
+    //MARK: - Delegate Functions
+    //#5 conform to the protocol
+    func timerSecondTick() {
+        // we just have to update the timer label, no need to update the entire view
+        updateTimerLabel()
+    }
+    
+    func timerCompleted() {
+        // when timer completes we want to pop up an alert, and update view
+        updateView()
+        presentAlertController()
+       
+    }
+    
+    func timerStopped() {
+        updateView()
+    }
+    
+    
+    // MARK: - notifications?
+    
+    func presentAlertController(){
+        // we need a textfield that is accesible in this whole function
+        var snoozeTextField: UITextField?
+        
+        let alertController = UIAlertController(title: "Wake Up", message: "Time to Wake up", preferredStyle: .alert)
+        //add text field for alert
+        alertController.addTextField { (textField) in
+            //add placeholder text
+            textField.placeholder = "pass in more minutes"
+            //specifie keybord type
+            textField.keyboardType = .numberPad
+            //we need to assign it t the global text field bc this one is locekd in the closure
+            snoozeTextField = textField
+        }
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+        let snoozeAction = UIAlertAction(title: "Snooze", style: .default) { (_) in
+            // we need the text field to get the amout of minutes the usr want to snooze for
+           guard let snoozeTime =  snoozeTextField?.text,
+            let time = TimeInterval(snoozeTime) else {return}
+            //start the timer again annded 1 so it would update the view faster
+            TimerController.shared.startTimer(time: time + 1)
+            //update view
+            self.updateView()
+        }
+        // add the actions to the alert
+        alertController.addAction(dismissAction)
+        alertController.addAction(snoozeAction)
+        
+        present(alertController, animated: true)
+        
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
 
